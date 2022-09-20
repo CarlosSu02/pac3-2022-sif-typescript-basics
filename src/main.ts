111fb1db26b4e1aa693fea17c4baf6fecb362014
CarlosSu02 <carlossuplei@gmail.com>
2022-09-19 23:47:56 -0600

    class types in types.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 
-console.log('Hello world!, May the force be with you!');
+// console.log('Hello world!, May the force be with you!');
+
+import Types from "./types";
+
+const types = new Types(0, 0);
+
+console.log(`The result of the sum of 5 and 5 is: ${types.sum(5, 5)}`)
+console.log(`The result of the subtraction of 5 and 5 is: ${types.subtraction(5, 5)}`)
+console.log(`The result of the multiplication of 5 and 5 is: ${types.multiplication(5, 5)}`)
+console.log(`The result of the division of 5 and 5 is: ${types.division (5, 5)}`)
+
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,41 @@
 
+export default class Types {
+
+    number1: number;
+    number2: number;
+    // result: number;
+
+    constructor(number1: number, number2: number) {
+
+        this.number1 = number1;
+        this.number2 = number2;
+        
+    }
+
+    sum(number1: number, number2: number): number {
+
+        return number1 + number2;
+
+    }
+
+    subtraction(number1: number, number2: number): number {
+
+        return number1 - number2;
+
+    }
+
+    multiplication(number1: number, number2: number): number {
+
+        return number1 * number2;
+
+    }
+
+    division (number1: number, number2: number): number | string{
+
+        // return number1 / number2;
+
+        return (number2 !== 0) ? number1 / number2 : `Cannot be divided by 0!`;
+
+    }
+
+};
